f06946c9b587890d03cab1b8da81aeb68523504d
BartPu <bpuchalski@tlen.pl>
2017-08-08 22:23:27 +0200

    commit all

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = 'Kasia';
+
+if (allNames.indexOf(newName) === -1) {
+    var allNames = allNames.push(newName);
+    console.log(allNames);
+} else {
+    console.log('Already exists in the array, pls. enter new name.');
+};
